d2d7ce39a6f1559e14b4490816b782c8592171ad
Eugeniu Zaicanu <eugeniu.zaicanu@avira.com>
2014-10-05 22:14:12 +0200

    Added pre-serialize and post-deserialize hooks

--- exchanger/messages.py
+++ exchanger/messages.py
@@ -4,8 +4,15 @@ import struct
 
 class Message(object):
 
+    def pre_serialize(self, message):
+        return message
+
+    def post_deserialize(self, message):
+        return message
+
     def serialize(self, protocol=cPickle.HIGHEST_PROTOCOL):
         message = cPickle.dumps(self.__dict__, protocol)
+        message = self.pre_serialize(message)
         len_message = len(message)
         payload = struct.pack('!i%ds' % len_message, len_message, message)
         return payload
@@ -13,6 +20,7 @@ class Message(object):
     def deserialize(self, payload):
         message_length = struct.unpack_from('!i', payload, 0)[0]
         message = struct.unpack_from('!%ds' % message_length, payload, 4)[0]
+        message = self.post_deserialize(message)
         state = cPickle.loads(message)
         self.__dict__.update(state)
 
